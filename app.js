971145d091a8406006289e5275fdb820e8cae44f
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-04-18 14:57:23 +0100

    end point for viewing all users and endpoint for sepcific user using username

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const logger = require("./logger");
 
+const users = require('./users.json');
+
 const app = express();
 app.use(cors());
 app.use(express.json())
@@ -13,6 +15,20 @@ app.get('/', (req, res) => {
     res.send('hi');
 })
 
+app.get('/users' , (req , res)=>{
 
+   res.send(users)
+
+})
+
+app.get('/users/:username', (req, res) => {
+    const username = req.params.username;
+    const user = users.find(user => user.username === username);
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
+    } else {
+        res.json(user);
+    }
+});
 
 module.exports = app
