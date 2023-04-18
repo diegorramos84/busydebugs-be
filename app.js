20c1bd6d6beba7f4afb424bfeb0596960e6c7ea3
Diego Ramos <diegorramos84@gmail.com>
2023-04-18 11:57:01 +0100

    add express, nodemon, cors and basic server code

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require("./logger");
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.use(logger);
+
+
+app.get('/', (req, res) => {
+    res.send('hi');
+})
+
+module.exports = app
